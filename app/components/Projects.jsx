7f2e4ec0db201a0ab675113a0b9baba34f362a0a
ppe11 <panphooeain@Pans-MacBook-Air-4.local>
2025-02-11 00:38:36 -0800

    Add project cards to projects section

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -11,7 +11,23 @@ const Projects = (isDarkMode) => {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus numquam laborum corrupti ipsa. Magni obcaecati, 
             reiciendis suscipit quidem hic repellat accusantium, inventore debitis labore ab architecto dolorem voluptatum recusandae harum.
             </p>
-
+            
+            <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
+                {workData.map((project, index) => (
+                    <div
+                    key={index}
+                    className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
+                    style={{ backgroundImage: `url(${project.bgImage})` }}>
+                    <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 
+                    flex items-center justify-between duration-500 group-hover:bottom-10'>
+                        <div>
+                            <h2 className='font-semibold'>{project.title}</h2>
+                            <p className='text-sm text-gray-700'>{project.description}</p>
+                        </div>
+                    </div>
+                    </div>
+                ))}
+            </div>
       </div>
     )
   }
