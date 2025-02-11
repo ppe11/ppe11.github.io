8d43362ae682d5331b0567389103eff04c4d54a0
ppe11 <panphooeain@Pans-MacBook-Air-4.local>
2025-02-11 01:15:20 -0800

    Add social media links to footer section

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -13,6 +13,11 @@ const Footer = () => {
         </div>
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
             <p>© 2025 Pan Phoo Eain. All rights reserved</p>
+            <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                <li><a target='_blank' href="https://github.com/ppe11">GitHub</a></li>
+                <li><a target='_blank' href="https://www.linkedin.com/in/pan-phoo-eain-119768350/">LinkedIn</a></li>
+                <li><a target='_blank' href="https://www.instagram.com/panphooeain16/">Instagram</a></li>
+            </ul>
         </div>
     </div>
   )
