76f4d6a608ea6f9e13aafb51567272fa2a0535b0
ppe11 <panphooeain@Pans-MacBook-Air-4.local>
2025-02-10 19:53:11 -0800

    Add assets folder and public files

---
+++ assets /assets.js
@@ -0,0 +1,41 @@
+
+import code_icon from './code-icon.png';
+import edu_icon from './edu-icon.png';
+import vscode from './vscode.png';
+import figma from './figma.png';
+import git from './git.png';
+import right_arrow_white from './right-arrow-white.png';
+import mail_icon from './mail_icon.png';
+import profile_img from './mypicture.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import project_icon from './project-icon.png';
+
+export const assets = {
+    project_icon,
+    code_icon,
+    edu_icon,
+    vscode,
+    figma,
+    git,
+    right_arrow_white,
+    mail_icon,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+};
+
