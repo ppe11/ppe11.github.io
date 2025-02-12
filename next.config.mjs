7a79081f6d3031b724e1a8119ab981b52d940e0e
ppe11 <panphooeain@Pans-MacBook-Air-4.local>
2025-02-11 18:26:06 -0800

    Update next.config.mjs and package.json to enable static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    basePath: "",
+    assetPrefix: "/"
+  };
+  
+  export default nextConfig;
+  
